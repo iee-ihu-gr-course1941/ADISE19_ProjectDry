9195f06ba8ea9f08a018b30da346a0e5ae929d1c
glauvus <glauvus@hotmail.com>
2019-12-10 00:41:46 +0200

    updated dry.php to handle /player requests

--- dry.php
+++ dry.php
@@ -13,19 +13,38 @@ switch ($r=array_shift($request)) {
         case 'board' : 
                 switch ($b=array_shift($request)) {
                         case '':
-                        case null: handle_board($method);
+                        case null:
+                                handle_board($method);
                                 break;
-                        default: header("HTTP/1.1 404 Not Found");
+                        default:
+                                header("HTTP/1.1 404 Not Found");
                                 break;
                 }
                 break;
         case 'status': 
-		if(sizeof($request)==0) {show_status();}
-		else {header("HTTP/1.1 404 Not Found");}
+                if(sizeof($request)==0)
+                        show_status();
+                else
+                        header("HTTP/1.1 404 Not Found");
 		break;
-        case 'players': handle_player($method, $request, $input);
-		break;
-	default:  header("HTTP/1.1 404 Not Found");
+        case 'players':
+                switch ($b=array_shift($request)) {
+                        case '':
+                        case null:
+                                handle_players($method);
+                                break;
+                        case 1:
+                        case 2:
+                                handle_user($method, $b, $input);
+                                break;
+                        default:
+                                header("HTTP/1.1 404 Not Found");
+                                print json_encode(['errormesg'=>"Player $b not found."]);
+                                break;
+                }
+                break;
+        default:
+                header("HTTP/1.1 404 Not Found");
         exit;
 }
 
@@ -33,14 +52,34 @@ switch ($r=array_shift($request)) {
 //function implementation
 
 function handle_board($method) {
-        if($method=='GET') {
+        if($method=='GET')
                 show_board();
-        } else if ($method=='POST') {
+        else if ($method=='POST')
                 reset_board();
+        else {
+                header("HTTP/1.1 400 Bad Request");
+                print json_encode(['errormesg'=>"Method $method not allowed."]);
         }		
 }
-function handle_player($method, $request, $input) {
-        //TODO
+
+function handle_players($method) {
+        if($method=='GET')
+                show_players();
+        else {
+                header("HTTP/1.1 400 Bad Request");
+                print json_encode(['errormesg'=>"Method $method not allowed."]);
+        }
 }
- 
+
+function handle_user($method, $b, $input) {
+        if($method=='GET')
+                show_user($b);
+        else if($method=='PUT')
+                set_user($b,$input);
+        else {
+                header("HTTP/1.1 400 Bad Request");
+                print json_encode(['errormesg'=>"Method $method not allowed."]);
+        }
+}
+
 ?>
